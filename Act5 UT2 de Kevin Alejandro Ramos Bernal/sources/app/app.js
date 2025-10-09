a99fe453cf9658ed30aa8ee3d4e60f81b6805971
Kevin Ramos <182262851+Peti-sui@users.noreply.github.com>
2025-10-09 13:21:31 +0100

    Add files via upload
    Se han añadido estilos, correcto funcionamiento y tratado de errores comprobando por segunda vez e impidiendo el refresco hasta que se le haya dado a enter.

--- Act5 UT2 de Kevin Alejandro Ramos Bernal/sources/app/app.js
+++ Act5 UT2 de Kevin Alejandro Ramos Bernal/sources/app/app.js
@@ -1,14 +1,43 @@
+
+/* declaracion del DOM */
 const numero = document.getElementById("numeroInput").value;
 let decenas_res = document.getElementById("resultado-decenas");
 let unidades_res = document.getElementById("resultado-unidades");
 
+/* inicializaciones para el tratado de las mismas*/
+let decenas = 0;
 
-let decenas = numero / 10;
+let unidades = 0;
 
-let unidades = numero % 10;
 
-decenas_res.innerHTML = `Decenas:  ${decenas}`;
-unidades_res_res.innerHTML = `Unidades:  ${unidades}`;
+numeroInput.addEventListener('keydown', function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        mostrar_resultados();
+    }
+});
+
+/* metodo o funcion para mostrar los resultados */
+function mostrar_resultados() {
+    const numero = parseInt(numeroInput.value);
+
+    /* valida el rango de numeros para asegurarse por segunda vez */
+    if (numero < 10 || numero > 99 || isNaN(numero)) {
+        decenas_res.innerHTML = "Por favor introduce un numero entre 10 y 99";
+        unidades_res.innerHTML = "";
+        /* si no se cumple se retorna y sale para no hacer el calculo */
+        return;
+    }
+/* se realiza el calculo*/
+    const decenas = parseInt(numero / 10);
+    const unidades = numero % 10;
+/* se muestran los resultados mediante innerHTML */
+    decenas_res.innerHTML = `Decenas: ${decenas}`;
+    unidades_res.innerHTML = `Unidades: ${unidades}`;
+}
+
+
+
 
 
 
